77acc80b693eac435e202480c98f4ca15991ae86
Maxim <azimuth70@gmail.com>
2021-06-02 19:33:38 +0700

    Нужно больше функций v.1.1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 
-let getRandomNumber = function (minNumber, maxNumber) {
+const getRandomNumber = function (minNumber, maxNumber) {
 
-  if (minNumber === maxNumber) {
-    return console.log('Значения диапазона не могут быть равны между собой.');}
-  if (minNumber > maxNumber) {
-    return console.log('Начальное значение диапазона должно быть меньше, чем конечное.');}
-
-  return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
-}
+  if (minNumber < maxNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+  }
+  console.log('Начальное значение диапазона не должно быть больше (либо равно) конечному.');
+};
 
 getRandomNumber (0, 0);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
-let getRandomFractional = function (minNumber, maxNumber, numberCut) {
+const getRandomFractional = function (minNumber, maxNumber, numberCut) {
 
   if (minNumber === maxNumber) {
-    return console.log('Значения диапазона не могут быть равны между собой.');}
+    console.log('Значения диапазона не могут быть равны между собой.');
+  }
+
   if (minNumber > maxNumber) {
-    return console.log('Начальное значение диапазона должно быть меньше, чем конечное.');}
+    console.log('Начальное значение диапазона должно быть меньше, чем конечное.');
+  }
 
   return (Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numberCut);
-}
+};
 
 getRandomFractional (0, 0, 0);
